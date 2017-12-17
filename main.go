389fea532c93f560624801199ef625317548d377
mozillazg <mozillazg101@gmail.com>
2017-12-17 20:23:56 +0800

    add `-s`, `-u`, `-p` to set servers and auth

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -14,11 +15,20 @@ import (
 const version = "0.1.0"
 
 func main() {
-	args := os.Args[1:]
-	conn, _, err := zk.Connect([]string{"127.0.0.1"}, time.Second)
+	servers := flag.String("s", "127.0.0.1:2181", "Servers")
+	username := flag.String("u", "", "Username")
+	password := flag.String("p", "", "Password")
+	flag.Parse()
+	args := flag.Args()
+
+	conn, e, err := zk.Connect(strings.Split(*servers, ","), time.Second)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
+	}
+	if *username != "" && *password != "" {
+		authS := fmt.Sprintf("%s:%s", *username, *password)
+		conn.AddAuth("digest", []byte(authS))
 	}
 	if len(args) > 0 {
 		c := zkcli.ParseCmd(strings.Join(args, " "))
@@ -26,6 +36,16 @@ func main() {
 		c.Conn = conn
 		c.Run()
 		return
+	}
+
+loop:
+	for {
+		select {
+		case event, ok := <-e:
+			if ok && event.State == zk.StateConnected {
+				break loop
+			}
+		}
 	}
 
 	p := prompt.New(
